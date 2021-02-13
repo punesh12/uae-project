bc320070a210aa32cfb8f1d361cad2b1cb2ee3db
Punesh <punesh12@gmail.com>
2021-02-13 16:33:59 +0530

    final

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,45 +1,65 @@
-import React from 'react'
+import emailjs, { init } from "emailjs-com";
+
 
 function Contact() {
+  init("user_Hm9Oh3Ko4qjB14auFKmUm");
+
+    function sendEmail(e){
+            e.preventDefault();
+            emailjs.sendForm('service_2368d39', 'template_vz2oeha', e.target,'user_Hm9Oh3Ko4qjB14auFKmUm')
+            .then((result)=>{
+                console.log('succ', result.text);
+                console.log(navigator)
+
+            }, (error) =>{
+                console.log(error.text)
+            }
+            )
+    }
+
+    
     return (
         <div>
             <section class="text-gray-600 body-font relative">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-12">
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+      {/* <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p> */}
     </div>
-    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+    <form class="lg:w-1/2 md:w-2/3 mx-auto" onSubmit={sendEmail}>
       <div class="flex flex-wrap -m-2">
-        <div class="p-2 w-full">
-          <div class="relative">
-            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" placeholder="Elon musk" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-          </div>
-        </div>
-        <div class="p-2 w-full">
-          <div class="relative">
-            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" placeholder="elon@mail.com" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-          </div>
-        </div>
-        <div class="p-2 w-full">
-          <div class="relative">
-            <label for="email" class="leading-7 text-sm text-gray-600" >Contact Number</label>
-            <input type="email" id="email" name="email" placeholder="+1- 123 456 7890" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-          </div>
-        </div>  
-        <div class="p-2 w-full">
-          <div class="relative">
-            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-          </div>
-        </div>
+            <div class="p-2 w-full">
+              <div class="relative">
+                <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+                <input type="text" id="name" name="user_name" placeholder="Elon musk" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+              </div>
+            </div>
+            <div class="p-2 w-full">
+              <div class="relative">
+                <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+                <input type="email" id="email" name="user_email" placeholder="elon@mail.com" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+              </div>
+            </div>
+            <div class="p-2 w-full">
+              <div class="relative">
+                <label for="email" class="leading-7 text-sm text-gray-600" >Contact Number</label>
+                <input type="text" id="number" name="user_contact" placeholder="+1- 123 456 7890" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+              </div>
+            </div>  
+            <div class="p-2 w-full">
+              <div class="relative">
+                <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+                <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+              </div>
+            </div>
+            <input type="hidden" name="os" value={navigator.oscpu} />
+        <input type="hidden" name="browser" value={navigator.appCodeName} />
+
         <div class="p-2 w-full">
           <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Submit</button>
         </div>
         <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-          <a class="text-green-500">example@email.com</a>
+          {/* <a class="text-green-500">example@email.com</a> */}
           <p class="leading-normal my-5">49 Smith St.
             <br/>Saint Cloud, MN 56301
           </p>
@@ -68,7 +88,7 @@ function Contact() {
           </span>
         </div>
       </div>
-    </div>
+    </form>
   </div>
 </section>
             
